4db4f3e8b96c6499afe227d947de841b51378d81
andri <aarfianto11@gmail.com>
2021-07-24 18:37:13 +0700

    Home Screen - Add instashayna component

--- src/components/HeroShayna.vue
+++ src/components/HeroShayna.vue
@@ -1,69 +1,67 @@
 <template>
-  <div>
-    <!-- Hero Section Begin -->
-    <section class="hero-section">
-      <carousel class="hero-items" :items="1" :nav="false">
-        <div
-          class="single-hero-items set-bg"
-          style="background-image: url('img/hero-1.jpg')"
-        >
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-5">
-                <span>Bag,kids</span>
-                <h1>Black friday</h1>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing
-                  elit, sed do eiusmod tempor incididunt ut labore et
-                  dolore
-                </p>
-                <a href="#" class="primary-btn">Shop Now</a>
-              </div>
+  <!-- Hero Section Begin -->
+  <section class="hero-section">
+    <carousel class="hero-items" :items="1" :nav="false">
+      <div
+        class="single-hero-items set-bg"
+        style="background-image: url('img/hero-1.jpg')"
+      >
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-5">
+              <span>Bag,kids</span>
+              <h1>Black friday</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing
+                elit, sed do eiusmod tempor incididunt ut labore et
+                dolore
+              </p>
+              <a href="#" class="primary-btn">Shop Now</a>
             </div>
           </div>
         </div>
-        <div
-          class="single-hero-items set-bg"
-          style="background-image: url('img/hero-2.jpg')"
-        >
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-5">
-                <span>Bag,kids</span>
-                <h1>Black friday</h1>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing
-                  elit, sed do eiusmod tempor incididunt ut labore et
-                  dolore
-                </p>
-                <a href="#" class="primary-btn">Shop Now</a>
-              </div>
+      </div>
+      <div
+        class="single-hero-items set-bg"
+        style="background-image: url('img/hero-2.jpg')"
+      >
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-5">
+              <span>Bag,kids</span>
+              <h1>Black friday</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing
+                elit, sed do eiusmod tempor incididunt ut labore et
+                dolore
+              </p>
+              <a href="#" class="primary-btn">Shop Now</a>
             </div>
           </div>
         </div>
-        <div
-          class="single-hero-items set-bg"
-          style="background-image: url('img/hero-3.jpg')"
-        >
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-5">
-                <span>Bag,kids</span>
-                <h1>Black friday</h1>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing
-                  elit, sed do eiusmod tempor incididunt ut labore et
-                  dolore
-                </p>
-                <a href="#" class="primary-btn">Shop Now</a>
-              </div>
+      </div>
+      <div
+        class="single-hero-items set-bg"
+        style="background-image: url('img/hero-3.jpg')"
+      >
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-5">
+              <span>Bag,kids</span>
+              <h1>Black friday</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing
+                elit, sed do eiusmod tempor incididunt ut labore et
+                dolore
+              </p>
+              <a href="#" class="primary-btn">Shop Now</a>
             </div>
           </div>
         </div>
-      </carousel>
-    </section>
-    <!-- Hero Section End -->
-  </div>
+      </div>
+    </carousel>
+  </section>
+  <!-- Hero Section End -->
 </template>
 
 <script>
@@ -74,5 +72,5 @@ export default {
   components: {
     carousel,
   },
-}
+};
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
     <HeaderShayna />
     <HeroShayna />
     <WomanShayna />
+    <InstaShayna />
   </div>
 </template>
 
@@ -11,6 +12,7 @@
 import HeaderShayna from '@/components/HeaderShayna.vue';
 import HeroShayna from '@/components/HeroShayna.vue';
 import WomanShayna from '@/components/WomanShayna.vue';
+import InstaShayna from '@/components/InstaShayna.vue';
 
 export default {
   name: 'Home',
@@ -18,6 +20,7 @@ export default {
     HeaderShayna,
     HeroShayna,
     WomanShayna,
+    InstaShayna,
   },
 };
 </script>
