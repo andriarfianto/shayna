ca045096efd4eddbc9050044030043fbd517c0fa
andri <aarfianto11@gmail.com>
2021-07-24 17:22:36 +0700

    add carousel component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <Header />
+    <HeroShayna />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Header from '@/components/Header.vue'
+import Header from '@/components/Header.vue';
+import HeroShayna from '@/components/HeroShayna.vue';
 
 export default {
   name: 'Home',
   components: {
-    Header
-  }
-}
+    Header,
+    HeroShayna
+  },
+};
 </script>
