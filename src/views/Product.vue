a24fb04fc75013b7f52ad22600570f8016e8e286
andri <aarfianto11@gmail.com>
2021-07-24 20:40:33 +0700

    Product - Create vue file

---
+++ src/views/Product.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShayna from '@/components/HeaderShayna.vue';
+import FooterShayna from '@/components/FooterShayna.vue';
+
+export default {
+  name: 'Product',
+  components: {
+    HeaderShayna,
+    FooterShayna,
+  },
+};
+</script>
